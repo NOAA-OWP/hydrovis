24de01567caf49924135f780b846cd155b87b5e7
nickchadwick-noaa <nick.chadwick@noaa.gov>
2022-01-19 13:20:43 -0600

    ignored tags in Viz EC2s, changed viz inundation parent ENV variable

--- Core/LAMBDA/viz_functions/main.tf
+++ Core/LAMBDA/viz_functions/main.tf
@@ -172,7 +172,7 @@ resource "aws_lambda_function" "viz_inundation_parent" {
       NWM_DATA_BUCKET             = var.nwm_data_bucket
       PROCESSED_OUTPUT_BUCKET     = var.fim_output_bucket
       PROCESSED_OUTPUT_PREFIX     = "processing_outputs"
-      RECURRENCE_FILENAME         = "viz/authoritative_data/derived_data/nwm_v21_recurrence_flows/nwm_v21_recurrence_flows_interim.nc"
+      RECURRENCE_FILENAME         = "viz/authoritative_data/derived_data/nwm_v21_recurrence_flows/nwm_v20_recurrence_flows_slim.nc"
       RECURRENCE_BANKFULL         = "1_5_year_recurrence_flow"
       RECURRENCE_HAWAII_FILENAME  = "viz/authoritative_data/derived_data/nwm_v21_recurrence_flows/nwm_v20_recurrence_flows_hawaii.nc"
       RECURRENCE_HAWAII_BANKFULL  = "2_0_year_recurrence_flow"
