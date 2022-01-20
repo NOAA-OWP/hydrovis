92441872b6d000b5ea3d53631db270ee1a44014f
Richard Corbett <richard.corbett@noaa.gov>
2022-01-20 15:02:49 -0600

    fixing typos in reference to security group modules in main.tf

--- Core/main.tf
+++ Core/main.tf
@@ -187,7 +187,7 @@ module "nginx_load_balancer" {
   source = "./LoadBalancer/nginx"
 
   environment     = local.env.environment
-  security_groups = [module.security_groups.hv-allow-NWC-access]
+  security_groups = [module.security-groups.hv-allow-NWC-access]
   subnets         = [module.vpc.subnet_hydrovis-sn-prv-web1a.id, module.vpc.subnet_hydrovis-sn-prv-web1b.id]
   vpc             = module.vpc.vpc_main.id
   certificate_arn = local.env.load_balancer_certificare_arn
@@ -489,5 +489,5 @@ module "nginx_fargate" {
   kibana_endpoint   = module.monitoring.aws_elasticsearch_domain.kibana_endpoint
   load_balancer_tg  = module.nginx_load_balancer.aws_lb_target_group_kibana_ngninx.arn
   subnets           = [module.vpc.subnet_hydrovis-sn-prv-web1a.id, module.vpc.subnet_hydrovis-sn-prv-web1b.id]
-  security_groups   = [module.security_groups.hv-allow-NWC-access]
+  security_groups   = [module.security-groups.hv-allow-NWC-access]
 }
