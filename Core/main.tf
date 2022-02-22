91d23ca702cf86f212bf40007956d285780ae3ab
NickChadwick-NOAA <nick.chadwick@noaa.gov>
2022-02-22 14:26:33 -0600

    Inundation metadata (#24)

--- Core/main.tf
+++ Core/main.tf
@@ -540,17 +540,3 @@ module "viz_ec2" {
   egis_db_name                = local.env.egis_db_name
   egis_db_secret_string       = module.secrets-manager.secret_strings["egis-pg-rds-secret"]
 }
-
-module "nginx_fargate" {
-  source = "./ECS/NGINX"
-
-  environment        = local.env.environment
-  region             = local.env.region
-  deployment_bucket  = module.s3.buckets["deployment"].bucket
-  es_domain_endpoint = module.monitoring.aws_elasticsearch_domain.endpoint
-  load_balancer_tg   = module.nginx_listener.aws_lb_target_group_kibana_ngninx.arn
-  subnets            = [module.vpc.subnet_hydrovis-sn-prv-web1a.id, module.vpc.subnet_hydrovis-sn-prv-web1b.id]
-  security_groups    = [module.security-groups.hv-allow-kibana-access.id]
-  iam_role_arn       = module.iam-roles.role_hydrovis-ecs-resource-access.arn
-  ecs_execution_role = module.iam-roles.role_ecs-task-execution.arn
-} 
