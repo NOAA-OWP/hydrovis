43fabef28022210e9f9e148e3d8dd9bda9040d42
nick.chadwick <nick.chadwick@noaa.gov>
2022-03-02 09:37:45 -0600

    Cleaned up json and fixed some syntax errors and template variable

--- Core/eGIS/main.tf
+++ Core/eGIS/main.tf
@@ -65,7 +65,7 @@ locals {
         var.role_autoscaling_arn,
         var.role_HydrovisESRISSMDeploy_arn
       ]
-      "policy_filename" = "portalcontent_S3_bucket_policy.json"
+      "policy_filename" = "portalcontent_S3_bucket_policy.json.tftpl"
     }
     "pub-alb-logging" = {
       "access_principal_arns" = [
@@ -79,7 +79,7 @@ locals {
         var.role_autoscaling_arn,
         var.role_HydrovisESRISSMDeploy_arn
       ]
-      "policy_filename" = "webgisdr_S3_bucket_policy.json"
+      "policy_filename" = "webgisdr_S3_bucket_policy.json.tftpl"
     }
   }
 }
