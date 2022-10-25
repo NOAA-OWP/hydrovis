f79709154643b2ea9432680d74e0bd372adf5ec1
Corey Krewson <corey.krewson@noaa.gov>
2022-10-25 12:56:34 -0500

    removed old zip based update egis data lambda function

--- Core/LAMBDA/viz_functions/main.tf
+++ Core/LAMBDA/viz_functions/main.tf
@@ -124,10 +124,6 @@ variable "xarray_layer" {
   type = string
 }
 
-variable "geopandas_layer" {
-  type = string
-}
-
 variable "pandas_layer" {
   type = string
 }
@@ -512,61 +508,6 @@ resource "aws_lambda_function" "viz_fim_data_prep" {
 
 resource "aws_lambda_function_event_invoke_config" "viz_fim_data_prep_destinations" {
   function_name          = resource.aws_lambda_function.viz_fim_data_prep.function_name
-  maximum_retry_attempts = 0
-  destination_config {
-    on_failure {
-      destination = var.email_sns_topics["viz_lambda_errors"].arn
-    }
-  }
-}
-
-#############################
-##    Update EGIS Data     ##
-#############################
-
-resource "aws_lambda_function" "viz_update_egis_data" {
-  function_name = "viz_update_egis_data_${var.environment}"
-  description   = "Lambda function to copy a postprocesses service table into the egis postgreql database, as well as cache data in the viz database."
-  memory_size   = 6400
-  timeout       = 900
-  vpc_config {
-    security_group_ids = var.db_lambda_security_groups
-    subnet_ids         = var.db_lambda_subnets
-  }
-  environment {
-    variables = {
-      EGIS_DB_DATABASE    = var.egis_db_name
-      EGIS_DB_HOST        = var.egis_db_host
-      EGIS_DB_USERNAME    = jsondecode(var.egis_db_user_secret_string)["username"]
-      EGIS_DB_PASSWORD    = jsondecode(var.egis_db_user_secret_string)["password"]
-      VIZ_DB_DATABASE     = var.viz_db_name
-      VIZ_DB_HOST         = var.viz_db_host
-      VIZ_DB_USERNAME     = jsondecode(var.viz_db_user_secret_string)["username"]
-      VIZ_DB_PASSWORD     = jsondecode(var.viz_db_user_secret_string)["password"]
-      CACHE_BUCKET        = var.viz_cache_bucket
-      HYDROVIS_ENV        = var.environment
-    }
-  }
-  filename         = "${path.module}/viz_update_egis_data.zip"
-  source_code_hash = filebase64sha256("${path.module}/viz_update_egis_data.zip")
-  runtime          = "python3.9"
-  handler          = "lambda_function.lambda_handler"
-  role             = var.lambda_role
-  layers = [
-    var.geopandas_layer,
-    var.psycopg2_sqlalchemy_layer,
-    var.viz_lambda_shared_funcs_layer
-  ]
-  tags = {
-    "Name" = "viz_update_egis_data_${var.environment}"
-  }
-  ephemeral_storage {
-    size = 2048
-  }
-}
-
-resource "aws_lambda_function_event_invoke_config" "viz_update_egis_data_destinations" {
-  function_name          = resource.aws_lambda_function.viz_update_egis_data.function_name
   maximum_retry_attempts = 0
   destination_config {
     on_failure {
@@ -1110,7 +1051,7 @@ resource "aws_sfn_state_machine" "viz_pipeline_step_function" {
             "Type": "Task",
             "Resource": "arn:aws:states:::lambda:invoke",
             "Parameters": {
-              "FunctionName": "${aws_lambda_function.viz_update_egis_data.arn}",
+              "FunctionName": "arn:aws:lambda:${var.region}:${var.account_id}:function:${module.image_based_lambdas.update_egis_data}",
               "Payload": {
                 "args.$": "$",
                 "step": "update_summary_data"
@@ -1276,10 +1217,6 @@ output "fim_data_prep" {
   value = aws_lambda_function.viz_fim_data_prep
 }
 
-output "update_egis_data" {
-  value = aws_lambda_function.viz_update_egis_data
-}
-
 output "publish_service" {
   value = aws_lambda_function.viz_publish_service
 }
@@ -1302,4 +1239,8 @@ output "optimize_rasters" {
 
 output "raster_processing" {
   value = module.image_based_lambdas.raster_processing
+}
+
+output "update_egis_data" {
+  value = module.image_based_lambdas.update_egis_data
 }
