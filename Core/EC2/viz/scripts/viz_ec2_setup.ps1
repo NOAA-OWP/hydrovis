594e84ca696ef6619a4a031f9a8081eea2f6e52e
CoreyKrewson-NOAA <69687817+CoreyKrewson-NOAA@users.noreply.github.com>
2022-11-03 16:27:20 -0500

    Update viz_ec2_setup.ps1

--- Core/EC2/viz/scripts/viz_ec2_setup.ps1
+++ Core/EC2/viz/scripts/viz_ec2_setup.ps1
@@ -250,7 +250,8 @@ Set-Location -Path $AWS_SERVICE_REPO
 & $python_exe setup.py develop
 & "C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\conda.exe" install -y -n viz -c esri arcgis=2.0.0
 & "C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\conda.exe" install -y -n viz typing_extensions=4.1.1
-& $python_exe -m pip install geopandas==0.10.2
+& $python_exe -m pip install--upgrade setuptools
+& $python_exe -m pip install geopandas==0.10.2 psycopg2-binary==2.9.5 SQLAlchemy==1.4.42
 
 LogWrite "-->TRANFERRING AUTHORITATIVE DATA"
 $s3_authoritative = "s3://" + $DEPLOYMENT_DATA_BUCKET + "/" + $DEPLOY_FILES_PREFIX + "authoritative_data/"
