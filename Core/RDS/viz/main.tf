09b916acefc96f636e8c527955e8d09a42529b16
tyler.schrag <tyler.schrag@noaa.gov>
2023-01-05 21:40:52 -0800

    Updating Viz RDS Size & RDS Bastion Script to Load External Schema

--- Core/RDS/viz/main.tf
+++ Core/RDS/viz/main.tf
@@ -37,8 +37,8 @@ resource "aws_db_subnet_group" "viz-processing" {
 resource "aws_db_instance" "viz-processing" {
   identifier                   = "hydrovis-${var.environment}-viz-processing"
   db_name                      = var.viz_db_name
-  instance_class               = "db.r6g.xlarge"
-  allocated_storage            = 400
+  instance_class               = "db.m6g.xlarge"
+  allocated_storage            = 500
   storage_type                 = "gp2"
   engine                       = "postgres"
   engine_version               = "12.8"
