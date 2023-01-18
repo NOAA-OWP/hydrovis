bfaea1f2fda3ae31c3f5cba541a06037c41e0ada
Corey Krewson <corey.krewson@noaa.gov>
2023-01-18 10:54:34 -0600

    updated fim data sql files to create huc6 data files instead of huc8

--- Core/LAMBDA/viz_functions/viz_fim_data_prep/data_sql/srf_max_inundation_hi.sql
+++ Core/LAMBDA/viz_functions/viz_fim_data_prep/data_sql/srf_max_inundation_hi.sql
@@ -2,7 +2,7 @@ SELECT
     max_forecast.feature_id,
     max_forecast.maxflow_48hour_cms AS streamflow_cms,
     CONCAT(LPAD(crosswalk.huc8::text, 8, '0'), '-', crosswalk.branch_id) as huc8_branch,
-    LPAD(crosswalk.huc8::text, 8, '0') as huc,
+    LEFT(LPAD(crosswalk.huc8::text, 8, '0'), 6) as huc,
     crosswalk.hydro_id
 FROM cache.max_flows_srf_hi max_forecast
 JOIN derived.recurrence_flows_hi rf ON rf.feature_id=max_forecast.feature_id
