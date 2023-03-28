a2d568ab6f1570cdbdddee3782fb84945b753133
TylerSchrag-NOAA <97621365+TylerSchrag-NOAA@users.noreply.github.com>
2023-03-28 18:01:34 +0000

    Mar8 fixes (#360)
    * fixed sql issues with forecast having nwm_vers and reference time
    fixed streamflow feature id on end of popup
    fixed rfc max stage spatial reference
    
    * Adding ST_Transform to RFC Forecast Point SQL
    
    * updated service_metadata for production non-para services
    Fixed reference time for monitoring, added UTC to ingest data tables
    
    * Batch script to copy files from an s3 bucket in out account to another account.
    
    * Adding postgres user mapping to external usgs_gage table - this was thankfully easier than I thought.
    
    ---------
    
    Co-authored-by: Corey Krewson <corey.krewson@noaa.gov>

--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/summaries/rfc_5day_max_downstream_streamflow/rfc_points.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/summaries/rfc_5day_max_downstream_streamflow/rfc_points.sql
@@ -4,7 +4,7 @@ SELECT
     usgs_sitecode,
     nws_name,
     to_char('1900-01-01 00:00:00'::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS reference_time,
-    ST_SetSRID(ST_MakePoint( longitude, latitude), 4326) AS geom
+    ST_TRANSFORM(ST_SetSRID(ST_MakePoint(longitude, latitude),4326),3857) as geom
 INTO publish.rfc_5day_max_downstream_streamflow_rfc_points
 FROM ingest.ahps_metadata ahps
 INNER JOIN ingest.rnr_max_flows rfc ON ahps.nws_lid = rfc.nws_lid
