bad6deab39efe3864b36820946c6c69668798db4
CoreyKrewson-NOAA <69687817+CoreyKrewson-NOAA@users.noreply.github.com>
2023-04-14 12:01:02 -0500

    removed nwm 3.0 eventbridge triggers

--- Core/EventBridge/main.tf
+++ Core/EventBridge/main.tf
@@ -15,40 +15,6 @@ resource "aws_cloudwatch_event_rule" "scheduled_events" {
   schedule_expression = each.value.schedule_expression
 }
 
-#################################
-## Get Lambda Resource By Name ##
-#################################
-
-data "aws_lambda_function" "scheduled_rules_function" {
-  for_each = var.scheduled_rules
-
-  function_name = each.value.function_name
-}
-
-###############################
-## Initialize Lambda Target  ##
-###############################
-
-resource "aws_cloudwatch_event_target" "eventbridge_targets" {
-  for_each = var.scheduled_rules
-
-  rule      = aws_cloudwatch_event_rule.scheduled_events[each.key].name
-  target_id = data.aws_lambda_function.scheduled_rules_function[each.key].function_name
-  arn       = data.aws_lambda_function.scheduled_rules_function[each.key].arn
-}
-
-resource "aws_lambda_permission" "allow_eventbridge_targets" {
-  for_each = var.scheduled_rules
-
-  statement_id_prefix  = "AllowExecutionFromCloudWatch"
-  action               = "lambda:InvokeFunction"
-  function_name        = data.aws_lambda_function.scheduled_rules_function[each.key].function_name
-  principal            = "events.amazonaws.com"
-  source_arn           = aws_cloudwatch_event_rule.scheduled_events[each.key].arn
-}
-
-
-
 #############
 ## Outputs ##
 #############
