31085e296b6bd58b0f416350e41dda71aa2f3213
CoreyKrewson-NOAA <69687817+CoreyKrewson-NOAA@users.noreply.github.com>
2023-04-14 10:23:13 -0500

    fixed missing vars and unindented return for ana_snow_water_equiv

--- Core/LAMBDA/viz_functions/image_based/viz_raster_processing/services/ana_snow_water_equiv.py
+++ Core/LAMBDA/viz_functions/image_based/viz_raster_processing/services/ana_snow_water_equiv.py
@@ -4,6 +4,9 @@ sys.path.append("../utils")
 from lambda_function import open_raster, sum_rasters, create_raster, upload_raster
 
 def main(service_data, reference_time):
+    bucket = service_data["bucket"]
+    input_files = service_data["input_files"]
+    service_name = service_data['service']
 
     ### ABOVE THIS WILL BE THE SAME
 
@@ -21,4 +24,4 @@ def main(service_data, reference_time):
     raster_name = service_name
     uploaded_raster = upload_raster(reference_time, local_raster, service_name, raster_name)
 
-return [uploaded_raster] 
+    return [uploaded_raster] 
