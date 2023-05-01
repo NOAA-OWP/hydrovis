4803a073ec8b9b0cfb704591e51a79671ef7d047
Corey Krewson <corey.krewson@noaa.gov>
2023-05-01 16:50:06 -0500

    updated ana_72hr_accum_precip to ana_past_72hr_accum_precip

--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/products/forcing_analysis_assim/ana_past_72hr_accum_precip.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/products/forcing_analysis_assim/ana_past_72hr_accum_precip.sql
@@ -1,10 +1,10 @@
-DROP TABLE IF EXISTS publish.ana_72hr_accum_precip;
-CREATE TABLE publish.ana_72hr_accum_precip (
+DROP TABLE IF EXISTS publish.ana_past_72hr_accum_precip;
+CREATE TABLE publish.ana_past_72hr_accum_precip (
     reference_time TEXT,
     valid_time TEXT,
     update_time TEXT
 );
-INSERT INTO publish.ana_72hr_accum_precip
+INSERT INTO publish.ana_past_72hr_accum_precip
 VALUES (
     to_char('1900-01-01 00:00:00'::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC'), 
     to_char('1900-01-01 00:00:00'::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC'), 
