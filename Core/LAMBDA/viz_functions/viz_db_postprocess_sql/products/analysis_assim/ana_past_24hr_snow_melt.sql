f820a743185f62eb755354393e7ec9fe21f81323
Corey Krewson <corey.krewson@noaa.gov>
2023-05-01 16:29:45 -0500

    updated ana_24hr_snow_melt to ana_past_24hr_snow_melt

--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/products/analysis_assim/ana_past_24hr_snow_melt.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/products/analysis_assim/ana_past_24hr_snow_melt.sql
@@ -1,10 +1,10 @@
-DROP TABLE IF EXISTS publish.ana_24hr_snow_melt;
-CREATE TABLE publish.ana_24hr_snow_melt (
+DROP TABLE IF EXISTS publish.ana_past_24hr_snow_melt;
+CREATE TABLE publish.ana_past_24hr_snow_melt (
     reference_time TEXT,
     valid_time TEXT,
     update_time TEXT
 );
-INSERT INTO publish.ana_24hr_snow_melt
+INSERT INTO publish.ana_past_24hr_snow_melt
 VALUES (
     to_char('1900-01-01 00:00:00'::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC'), 
     to_char('1900-01-01 00:00:00'::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC'), 
