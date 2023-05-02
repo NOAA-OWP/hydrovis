2ea62e34f5dabac506c8478cd8b12a188dd84ffd
Corey Krewson <corey.krewson@noaa.gov>
2023-05-02 13:56:44 -0500

    updated srf_rapid_onset_flooding_probability to srf_12hr_rapid_onset_flooding_probability

--- Source/Visualizations/aws_loosa/pipelines/srf_12hr_rapid_onset_flooding_probability/srf_12hr_rapid_onset_flooding_probability_hucs.sql
+++ Source/Visualizations/aws_loosa/pipelines/srf_12hr_rapid_onset_flooding_probability/srf_12hr_rapid_onset_flooding_probability_hucs.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS publish.srf_rapid_onset_flooding_probability_hucs;
+DROP TABLE IF EXISTS publish.srf_12hr_rapid_onset_flooding_probability_hucs;
 SELECT
 	hucs.huc10,
 	TO_CHAR(hucs.huc10, 'fm0000000000') AS huc10_str,
@@ -13,8 +13,8 @@ SELECT
 	to_char(CAST(max(rofp.reference_time) AS timestamp) , 'YYYY-MM-DD HH24:MI:SS UTC') AS reference_time,
 	to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS update_time,
 	hucs.geom
-INTO publish.srf_rapid_onset_flooding_probability_hucs
+INTO publish.srf_12hr_rapid_onset_flooding_probability_hucs
 FROM derived.huc10s_conus AS hucs
 JOIN derived.featureid_huc_crosswalk AS crosswalk ON hucs.huc10 = crosswalk.huc10
-JOIN publish.srf_rapid_onset_flooding_probability AS rofp ON crosswalk.feature_id = rofp.feature_id
+JOIN publish.srf_12hr_rapid_onset_flooding_probability AS rofp ON crosswalk.feature_id = rofp.feature_id
 GROUP BY hucs.huc10, hucs.low_order_reach_count, hucs.total_low_order_reach_length, hucs.total_low_order_reach_miles, hucs.geom
