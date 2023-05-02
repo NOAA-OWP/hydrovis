74cb46b1e70ea92651ec0c4a5697f0f6b5e1609b
Corey Krewson <corey.krewson@noaa.gov>
2023-05-02 10:42:09 -0500

    updated mrf_accum_precip to mrf_gfs_10day_accum_precip

--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/products/forcing_medium_range/mrf_gfs_10day_accum_precip.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/products/forcing_medium_range/mrf_gfs_10day_accum_precip.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS publish.mrf_gfs_accum_precip;
-CREATE TABLE publish.mrf_gfs_accum_precip (
+DROP TABLE IF EXISTS publish.mrf_gfs_10day_accum_precip;
+CREATE TABLE publish.mrf_gfs_10day_accum_precip (
     reference_time TEXT,
     update_time TEXT
 );
-INSERT INTO publish.mrf_gfs_accum_precip
+INSERT INTO publish.mrf_gfs_10day_accum_precip
 VALUES (
     to_char('1900-01-01 00:00:00'::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC'),
     to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC')
