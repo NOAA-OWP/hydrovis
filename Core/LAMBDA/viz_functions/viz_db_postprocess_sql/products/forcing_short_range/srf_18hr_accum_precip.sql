cac3ca0a409b29e2cdaa98283216234cfa2e9af2
Corey Krewson <corey.krewson@noaa.gov>
2023-05-02 10:36:12 -0500

    updated srf_accum_precip to srf_18hr_accum_precip

--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/products/forcing_short_range/srf_18hr_accum_precip.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/products/forcing_short_range/srf_18hr_accum_precip.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS publish.srf_accum_precip;
-CREATE TABLE publish.srf_accum_precip (
+DROP TABLE IF EXISTS publish.srf_18hr_accum_precip;
+CREATE TABLE publish.srf_18hr_accum_precip (
     reference_time TEXT,
     update_time TEXT
 );
-INSERT INTO publish.srf_accum_precip
+INSERT INTO publish.srf_18hr_accum_precip
 VALUES (
     to_char('1900-01-01 00:00:00'::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC'),
     to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC')
