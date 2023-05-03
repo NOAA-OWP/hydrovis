3e184e3dbd9bee782a467df0b4d4bb350b324472
Corey Krewson <corey.krewson@noaa.gov>
2023-05-03 09:08:47 -0500

    updated branch_gms_catchments_hi to static_hand_catchments_gms_branches_hi_noaa

--- Source/Visualizations/aws_loosa/deploy/update_data_stores_and_sd_files.py
+++ Source/Visualizations/aws_loosa/deploy/update_data_stores_and_sd_files.py
@@ -19,7 +19,7 @@ service_dict = {
     "branch_0_catchments_hi": "fim_catchments",
     "branch_0_catchments_prvi": "fim_catchments",
     "branch_gms_catchments": "fim_catchments",
-    "branch_gms_catchments_hi": "fim_catchments",
+    "static_hand_catchments_gms_branches_hi_noaa": "fim_catchments",
     "branch_gms_catchments_prvi": "fim_catchments",
     "static_nwm_flowlines": "reference",
     "static_nwm_flowlines_hi": "reference",
