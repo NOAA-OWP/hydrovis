03e421220ed704a924624a6c435908f09a903312
Corey Krewson <corey.krewson@noaa.gov>
2023-05-03 11:13:23 -0500

    updated srf_max_inundation_hi to srf_48hr_max_inundation_extent_hi

--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/fim_configs/srf_48hr_max_inundation_extent_hi.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/fim_configs/srf_48hr_max_inundation_extent_hi.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS publish.srf_max_inundation_hi;
+DROP TABLE IF EXISTS publish.srf_48hr_max_inundation_extent_hi;
 
 SELECT  
 	inun.hydro_id,
@@ -17,8 +17,8 @@ SELECT
 	to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS update_time, 
 	derived.channels_hi.strm_order, 
     derived.channels_hi.name
-INTO publish.srf_max_inundation_hi
-FROM ingest.srf_max_inundation_hi as inun 
+INTO publish.srf_48hr_max_inundation_extent_hi
+FROM ingest.srf_48hr_max_inundation_extent_hi as inun 
 left join derived.channels_hi ON derived.channels_hi.feature_id = inun.feature_id
 --Add an empty row so that service monitor will pick up a reference and update time in the event of no fim features.
 UNION SELECT -9999, '-9999', 'NA', -9999, '-9999', -9999, -9999, -9999, -9999, 'NA', to_char('1900-01-01 00:00:00'::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC'),
