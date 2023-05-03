0c6cab7b9443d96d062b5499b81d03050ff670d0
Corey Krewson <corey.krewson@noaa.gov>
2023-05-03 13:25:01 -0500

    updated ana_inundation to ana_inundation_extent

--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/fim_configs/ana_inundation_extent.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/fim_configs/ana_inundation_extent.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS publish.ana_inundation;
+DROP TABLE IF EXISTS publish.ana_inundation_extent;
 
 SELECT  
 	inun.hydro_id,
@@ -18,6 +18,6 @@ SELECT
 	to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS update_time, 
 	derived.channels_conus.strm_order, 
     derived.channels_conus.name
-INTO publish.ana_inundation
-FROM ingest.ana_inundation as inun 
+INTO publish.ana_inundation_extent
+FROM ingest.ana_inundation_extent as inun 
 left join derived.channels_conus ON derived.channels_conus.feature_id = inun.feature_id;
