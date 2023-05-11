46f5d9836c8e77230800419ea5aa1097e424b5b5
Corey Krewson <corey.krewson@noaa.gov>
2023-05-11 16:08:49 -0500

    added update time to public fim services

--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/summaries/rfc_based_5day_max_inundation/public_subset.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/summaries/rfc_based_5day_max_inundation/public_subset.sql
@@ -4,7 +4,8 @@ SELECT
     inun.feature_id_str,
     inun.geom,
     inun.streamflow_cfs,
-    inun.reference_time
+    inun.reference_time,
+	to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS update_time
 INTO publish.rfc_based_5day_max_inundation_public
 FROM publish.rfc_based_5day_max_inundation as inun, derived.fim_domain as fim_domain
 WHERE ST_Intersects(inun.geom, fim_domain.geom)
