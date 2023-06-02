6f0662d0bb1137d5851181f43a9cfe5a9df8a95b
CoreyKrewson-NOAA <69687817+CoreyKrewson-NOAA@users.noreply.github.com>
2023-06-02 13:34:10 -0500

    Update srf_48hr_max_coastal_inundation_hi.sql
    removing valid time for non ana services

--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/fim_configs/srf_48hr_max_coastal_inundation_hi.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/fim_configs/srf_48hr_max_coastal_inundation_hi.sql
@@ -2,7 +2,6 @@ DROP TABLE IF EXISTS publish.srf_48hr_max_coastal_inundation_hi;
 
 SELECT
     inundation.*,
-    to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS update_time,
-    to_char('1900-01-01 00:00:00'::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS valid_time
+    to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS update_time
 INTO publish.srf_48hr_max_coastal_inundation_hi
 FROM ingest.srf_48hr_max_coastal_inundation_hi AS inundation;
