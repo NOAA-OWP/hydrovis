17ced7c14cf82e629e1e49393995b0a007b6b0cb
Nick Chadwick <nick.chadwick@noaa.gov>
2023-06-06 11:08:46 -0500

    removing references to non-existent SNS topics

--- Core/LAMBDA/viz_functions/main.tf
+++ Core/LAMBDA/viz_functions/main.tf
@@ -172,20 +172,6 @@ locals {
   ingest_flow_threshold = 0.001
 
   initialize_pipeline_subscriptions = toset([
-    "nwm_channel_ana",
-    "nwm_forcing_ana",
-    "nwm_channel_ana_hi",
-    "nwm_forcing_ana_hi",
-    "nwm_channel_ana_prvi",
-    "nwm_forcing_ana_prvi",
-    "nwm_channel_srf",
-    "nwm_forcing_srf",
-    "nwm_channel_srf_hi",
-    "nwm_forcing_srf_hi",
-    "nwm_channel_srf_prvi",
-    "nwm_forcing_srf_prvi",
-    "nwm_channel_mrf_10day",
-    "nwm_forcing_mrf",
     "rnr_max_flows"
   ])
 }
