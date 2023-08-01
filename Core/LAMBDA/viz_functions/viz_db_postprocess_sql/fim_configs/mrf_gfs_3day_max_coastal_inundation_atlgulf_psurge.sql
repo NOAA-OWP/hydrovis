6768d2fe27990e95abe47d87351489341176aedc
Corey Krewson <corey.krewson@noaa.gov>
2023-07-31 21:22:21 -0500

    removed update time from mrf coastal ingest empty row insertion

--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/fim_configs/mrf_gfs_3day_max_coastal_inundation_atlgulf_psurge.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/fim_configs/mrf_gfs_3day_max_coastal_inundation_atlgulf_psurge.sql
@@ -1,6 +1,6 @@
 INSERT INTO ingest.mrf_gfs_3day_max_coastal_inundation_atlgulf_psurge (
-	geom, reference_time, huc8, update_time)
-	VALUES (NULL, to_char('1900-01-01 00:00:00'::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC'), '-9999', to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC'));
+	geom, reference_time, huc8)
+	VALUES (NULL, to_char('1900-01-01 00:00:00'::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC'), '-9999');
 
 DROP TABLE IF EXISTS publish.mrf_gfs_3day_max_coastal_inundation_atlgulf_psurge;
 
