b3435f618ce5664708195af2d495b993e386fab0
Corey Krewson <corey.krewson@noaa.gov>
2023-08-09 10:41:36 -0500

    updated data sql files to only get feature id and streamflow

--- Core/LAMBDA/viz_functions/viz_fim_data_prep/data_sql/srf_48hr_max_inundation_hi.sql
+++ Core/LAMBDA/viz_functions/viz_fim_data_prep/data_sql/srf_48hr_max_inundation_hi.sql
@@ -1,14 +1,8 @@
 SELECT
     max_forecast.feature_id,
-    max_forecast.maxflow_48hour_cms AS streamflow_cms,
-    CONCAT(LPAD(crosswalk.huc8::text, 8, '0'), '-', crosswalk.branch_id) as huc8_branch,
-    LEFT(LPAD(crosswalk.huc8::text, 8, '0'), 6) as huc,
-    crosswalk.hydro_id
+    max_forecast.maxflow_48hour_cms AS streamflow_cms
 FROM cache.max_flows_srf_hi max_forecast
 JOIN derived.recurrence_flows_hi rf ON rf.feature_id=max_forecast.feature_id
-JOIN derived.fim4_featureid_crosswalk AS crosswalk ON rf.feature_id = crosswalk.feature_id
 WHERE 
     max_forecast.maxflow_48hour_cfs >= rf.high_water_threshold AND 
-    rf.high_water_threshold <> -9999 AND
-    crosswalk.huc8 IS NOT NULL AND 
-    crosswalk.lake_id = -999;
+    rf.high_water_threshold <> -9999
