c1b7cbff25e71072c5c799dc012d9a758ff8547e
Corey Krewson <corey.krewson@noaa.gov>
2023-08-17 12:00:42 -0500

    updated fim_data_prep sql files to remove fim4 specific lines

--- Core/LAMBDA/viz_functions/viz_fim_data_prep/data_sql/stage_based_catfim_action.sql
+++ Core/LAMBDA/viz_functions/viz_fim_data_prep/data_sql/stage_based_catfim_action.sql
@@ -1,10 +1,6 @@
 SELECT
     trace_feature_id as feature_id,
 	adj_action_stage_m as hand_stage_m,
-    CONCAT(LPAD(crosswalk.huc8::text, 8, '0'), '-', crosswalk.branch_id) as huc8_branch,
-    LEFT(LPAD(crosswalk.huc8::text, 8, '0'), 6) as huc,
-    crosswalk.hydro_id,
     nws_station_id
 FROM cache.rfc_categorical_stages AS rf
-LEFT JOIN derived.fim4_featureid_crosswalk AS crosswalk ON rf.trace_feature_id = crosswalk.feature_id
-WHERE crosswalk.huc8 IS NOT NULL AND crosswalk.lake_id = -999 AND adj_action_stage_m IS NOT NULL;
+WHERE adj_action_stage_m IS NOT NULL;
