e7ead2ac5274d01845b33cbd1c1b8cde50285d72
Corey Krewson <corey.krewson@noaa.gov>
2023-09-18 09:43:03 -0500

    updated fim_domain to be public_fim_domain for more clarity

--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/summaries/ana_inundation/public_subset.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/summaries/ana_inundation/public_subset.sql
@@ -8,6 +8,6 @@ SELECT
 	to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS update_time
 INTO publish.ana_inundation_public
 FROM publish.ana_inundation as inun
-JOIN derived.fim_domain as fim_domain ON ST_Intersects(inun.geom, fim_domain.geom)
+JOIN derived.public_fim_domain as fim_domain ON ST_Intersects(inun.geom, fim_domain.geom)
 JOIN derived.channels_conus cc ON cc.feature_id = inun.feature_id
 WHERE public_fim = true
