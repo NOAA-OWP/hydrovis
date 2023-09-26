92d9b0e08ae553a7f8928c2941f9c0646e1d985b
Corey Krewson <corey.krewson@noaa.gov>
2023-09-26 16:18:08 -0500

    added intersection to public subset sql files to clip geom at boundary

--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/summaries/ana_inundation/public_subset.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/summaries/ana_inundation/public_subset.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS publish.ana_inundation_public;
 
 SELECT
     inun.feature_id_str,
-    inun.geom,
+    ST_Intersection(inun.geom, fim_domain.geom) as geom,
     inun.streamflow_cfs,
     inun.reference_time,
 	to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS update_time
--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/summaries/mrf_gfs_10day_max_inundation/5day_public_subset.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/summaries/mrf_gfs_10day_max_inundation/5day_public_subset.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS publish.mrf_gfs_max_inundation_5day_public;
 
 SELECT
     inun.feature_id_str,
-    inun.geom,
+    ST_Intersection(inun.geom, fim_domain.geom) as geom,
     inun.streamflow_cfs,
     inun.reference_time,
 	to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS update_time
