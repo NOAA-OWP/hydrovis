206e616a5f9131748b6ddf1e092057bf43b69386
TylerSchrag-NOAA <97621365+TylerSchrag-NOAA@users.noreply.github.com>
2023-09-28 17:47:41 +0000

    Viz EC2 Services to Lambda - Part 1 - High Water Probability (#543)
    * Couple shared functions tweaks to work with high water probability
    
    * Changing viz_max_values lambda function viz_python_preprocessing and associated changes
    
    * High water probability product configuration and sql files
    
    * Remove high water probability services from Viz EC2
    
    * Adding a 'product' key to python_preprocessing configurations for better conditional logic.
    
    * increasing ephemeral strorage on python preprocess lambda to 6.5GB

---
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/products/short_range/srf_12hr_max_high_water_probability.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS publish.srf_12hr_max_high_water_probability;
+SELECT
+    channels.feature_id,
+    channels.feature_id::TEXT AS feature_id_str,
+    channels.name,
+    channels.strm_order,
+    channels.huc6,
+    channels.state,
+	hwp.nwm_vers,
+	hwp.reference_time,
+    hwp.srf_prob,
+    hwp.high_water_threshold,
+    to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS update_time,
+    channels.geom
+INTO publish.srf_12hr_max_high_water_probability
+FROM ingest.srf_12hr_max_high_water_probability as hwp
+JOIN derived.channels_conus channels ON hwp.feature_id = channels.feature_id;
--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/summaries/srf_12hr_max_high_water_probability/hucs.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/summaries/srf_12hr_max_high_water_probability/hucs.sql
@@ -1,15 +1,16 @@
+-- HUC10 Hotpsot Layer
 DROP TABLE IF EXISTS publish.srf_12hr_max_high_water_probability_hucs;
 SELECT
 	hucs.huc10,
 	TO_CHAR(hucs.huc10, 'fm0000000000') AS huc10_str,
 	hucs.total_nwm_features,
-	round(cast(count(bp.feature_id) / hucs.total_nwm_features * 100 as numeric), 2) AS nwm_features_flooded_percent,
-	round(avg(bp.srf_prob), 0) AS avg_prob,
-	to_char(CAST(max(bp.reference_time) AS timestamp) , 'YYYY-MM-DD HH24:MI:SS UTC') AS reference_time,
+	count(hwp.feature_id)::double precision / hucs.total_nwm_features::double precision AS nwm_features_flooded_percent,
+	avg(srf_prob) as avg_prob,
+	to_char('1900-01-01 00:00:00'::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS reference_time,
 	to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS update_time,
 	hucs.geom
 INTO publish.srf_12hr_max_high_water_probability_hucs
 FROM derived.huc10s_conus AS hucs
 JOIN derived.featureid_huc_crosswalk AS crosswalk ON hucs.huc10 = crosswalk.huc10
-JOIN publish.srf_12hr_max_high_water_probability AS bp ON crosswalk.feature_id = bp.feature_id
-GROUP BY hucs.huc10, hucs.total_nwm_features, hucs.geom
+JOIN publish.srf_12hr_max_high_water_probability AS hwp ON crosswalk.feature_id = hwp.feature_id
+GROUP BY hucs.huc10, total_nwm_features, hucs.geom
