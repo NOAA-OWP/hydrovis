d3dd5acf7ed8249cc70d284c3e5d8e894624ee77
tyler.schrag <tyler.schrag@noaa.gov>
2023-10-04 09:03:17 -0700

    Tweaks to high water probability summary sql

--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/summaries/mrf_gfs_5day_max_high_water_probability/hucs.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/summaries/mrf_gfs_5day_max_high_water_probability/hucs.sql
@@ -1,16 +1,16 @@
 -- HUC8 Hotpsot Layer
-DROP TABLE IF EXISTS publish.mrf_gfs_5day_max_high_water_probability_hucs;
+DROP TABLE IF EXISTS publish.mrf_gfs_5day_high_water_probability_hucs;
 SELECT
 	hucs.huc8,
 	TO_CHAR(hucs.huc8, 'fm00000000') AS huc8_str,
 	hucs.total_nwm_features,
-	count(hwp.feature_id)::double precision / hucs.total_nwm_features::double precision AS nwm_features_flooded_percent,
-	avg(hours_3_to_120) as avg_prob,
+	round(cast(count(hwp.feature_id) / hucs.total_nwm_features * 100 as numeric), 2) AS nwm_features_flooded_percent,
+	round(avg(hwp.hours_3_to_120), 0) AS avg_prob,
 	to_char('1900-01-01 00:00:00'::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS reference_time,
 	to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS update_time,
 	hucs.geom
-INTO publish.mrf_gfs_5day_max_high_water_probability_hucs
+INTO publish.mrf_gfs_5day_high_water_probability_hucs
 FROM derived.huc8s_conus AS hucs
 JOIN derived.featureid_huc_crosswalk AS crosswalk ON hucs.huc8 = crosswalk.huc8
-JOIN publish.mrf_gfs_5day_max_high_water_probability AS hwp ON crosswalk.feature_id = hwp.feature_id
-GROUP BY hucs.huc8, total_nwm_features, hucs.geom
+JOIN publish.mrf_gfs_5day_high_water_probability AS hwp ON crosswalk.feature_id = hwp.feature_id
+GROUP BY hucs.huc8, hucs.total_nwm_features, hucs.geom;
--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/summaries/srf_12hr_max_high_water_probability/hucs.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/summaries/srf_12hr_max_high_water_probability/hucs.sql
@@ -4,8 +4,8 @@ SELECT
 	hucs.huc10,
 	TO_CHAR(hucs.huc10, 'fm0000000000') AS huc10_str,
 	hucs.total_nwm_features,
-	count(hwp.feature_id)::double precision / hucs.total_nwm_features::double precision AS nwm_features_flooded_percent,
-	avg(srf_prob) as avg_prob,
+	round(cast(count(hwp.feature_id) / hucs.total_nwm_features * 100 as numeric), 2) AS nwm_features_flooded_percent,
+	round(avg(hwp.srf_prob), 0) AS avg_prob,
 	to_char('1900-01-01 00:00:00'::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS reference_time,
 	to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS update_time,
 	hucs.geom
@@ -13,4 +13,4 @@ INTO publish.srf_12hr_max_high_water_probability_hucs
 FROM derived.huc10s_conus AS hucs
 JOIN derived.featureid_huc_crosswalk AS crosswalk ON hucs.huc10 = crosswalk.huc10
 JOIN publish.srf_12hr_max_high_water_probability AS hwp ON crosswalk.feature_id = hwp.feature_id
-GROUP BY hucs.huc10, total_nwm_features, hucs.geom
+GROUP BY hucs.huc10, hucs.total_nwm_features, hucs.geom;
