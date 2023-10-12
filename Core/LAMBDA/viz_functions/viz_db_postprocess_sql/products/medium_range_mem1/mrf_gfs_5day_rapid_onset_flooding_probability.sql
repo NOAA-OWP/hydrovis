3b792fd7c846ac270ca4663b38f2f9d3a880fb4f
tyler.schrag <tyler.schrag@noaa.gov>
2023-10-12 10:21:35 -0700

    Shortening HWP and ROFP table name to support past event pipelines.

--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/products/medium_range_mem1/mrf_gfs_5day_rapid_onset_flooding_probability.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/products/medium_range_mem1/mrf_gfs_5day_rapid_onset_flooding_probability.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS publish.mrf_gfs_5day_rapid_onset_flooding_probability;
+DROP TABLE IF EXISTS publish.mrf_gfs_5day_rof_prob;
 SELECT
     channels.feature_id,
     channels.feature_id::TEXT AS feature_id_str,
@@ -17,7 +17,7 @@ SELECT
     ST_LENGTH(channels.geom)*0.000621371 AS reach_length_miles,
     to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS update_time,
     channels.geom
-INTO publish.mrf_gfs_5day_rapid_onset_flooding_probability
-FROM ingest.mrf_gfs_5day_rapid_onset_flooding_probability as rofp
+INTO publish.mrf_gfs_5day_rof_prob
+FROM ingest.mrf_gfs_5day_rof_prob as rofp
 JOIN derived.channels_conus channels ON rofp.feature_id = channels.feature_id
 JOIN derived.recurrence_flows_conus rf ON rofp.feature_id = rf.feature_id;
