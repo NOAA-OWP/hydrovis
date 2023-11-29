13c2d4f464783f00f0bb81bbda6f063d65c5ef14
Shawn Crawley <shawn.crawley@noaa.gov>
2023-11-29 08:20:14 -0700

    Adds dummy feature to service; Fixes UTC caps

--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/summaries/rfc_based_5day_max_inundation/public_subset.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/summaries/rfc_based_5day_max_inundation/public_subset.sql
@@ -8,4 +8,12 @@ SELECT
 	to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS update_time
 INTO publish.rfc_based_5day_max_inundation_public
 FROM publish.rfc_based_5day_max_inundation as inun
-JOIN derived.public_fim_domain as fim_domain ON ST_Intersects(inun.geom, fim_domain.geom)
+JOIN derived.public_fim_domain as fim_domain ON ST_Intersects(inun.geom, fim_domain.geom);
+
+INSERT INTO publish.rfc_based_5day_max_inundation_public (
+    reference_time, 
+    update_time
+) VALUES (
+    to_char('1900-01-01 00:00:00'::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC'), 
+    to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC')
+);
