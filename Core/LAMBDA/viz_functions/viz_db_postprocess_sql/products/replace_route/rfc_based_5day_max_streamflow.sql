e71b2c7941752b3b3cd959c449cfa64764d880ff
Shawn Crawley <shawn.crawley@noaa.gov>
2024-02-05 14:12:43 -0700

    Updates rnr to rely upon rfc_max_forecast service

--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/products/replace_route/rfc_based_5day_max_streamflow.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/products/replace_route/rfc_based_5day_max_streamflow.sql
@@ -31,86 +31,17 @@ max_flows_station_xwalk AS (
 		ON station.nws_station_id = xwalk.nws_station_id
 ),
 
-max_flows_station_xwalk_with_rc_stage AS (
-	SELECT
-		main.*, 
-		ROUND((curve.stage + ((curve.next_higher_point_stage - curve.stage) / (curve.next_higher_point_flow - curve.flow) * (main.streamflow - curve.flow)))::numeric, 2) as stage_from_curve
-	FROM max_flows_station_xwalk main
-	LEFT JOIN external.rating rating
-		ON rating.location_id = main.gage_id
-	LEFT JOIN rnr.staggered_curves curve
-		ON curve.rating_id = rating.rating_id
-		AND (curve.flow = main.streamflow
-			OR (curve.flow < main.streamflow 
-				AND curve.next_higher_point_flow > main.streamflow))
-),
-
-native_threshold AS (
-	SELECT DISTINCT ON (location_id)
-		location_id as nws_station_id,
-		rating_source,
-		action_stage,
-		minor_stage,
-		moderate_stage,
-		major_stage,
-		record_stage,
-		action_flow,
-		minor_flow,
-		moderate_flow,
-		major_flow,
-		record_flow
-	FROM external.threshold
-	WHERE rating_source = 'NONE'
-), usgs_threshold AS (
-	SELECT DISTINCT ON (location_id) 
-		location_id as nws_station_id,
-		rating_source,
-		action_stage,
-		minor_stage,
-		moderate_stage,
-		major_stage,
-		record_stage,
-		action_flow_calc as action_flow,
-		minor_flow_calc as minor_flow,
-		moderate_flow_calc as moderate_flow,
-		major_flow_calc as major_flow,
-		record_flow_calc as record_flow
-	FROM external.threshold
-	WHERE rating_source = 'USGS Rating Depot' AND location_id NOT IN (SELECT nws_station_id FROM native_threshold)
-), nrldb_threshold AS (
-	SELECT DISTINCT ON (location_id)
-		location_id as nws_station_id,
-		rating_source,
-		action_stage,
-		minor_stage,
-		moderate_stage,
-		major_stage,
-		record_stage,
-		action_flow_calc as action_flow,
-		minor_flow_calc as minor_flow,
-		moderate_flow_calc as moderate_flow,
-		major_flow_calc as major_flow,
-		record_flow_calc as record_flow
-	FROM external.threshold
-	WHERE rating_source = 'NRLDB' AND location_id NOT IN (SELECT nws_station_id FROM native_threshold UNION SELECT nws_station_id FROM usgs_threshold)
-), threshold AS (
-	SELECT * FROM native_threshold
-	UNION
-	SELECT * FROM usgs_threshold
-	UNION
-	SELECT * FROM nrldb_threshold
-),
-
 fcst_meta AS (
 	SELECT DISTINCT ON (lid, product_time) 
 		lid, 
-		product_time as issue_time
+		product_time as issue_time,
+		rating_curve_source
 	FROM rnr.domain_forecasts
 ),
 
 root_status_trace_reaches AS (
 	SELECT DISTINCT ON (feature_id)
-		feature_id, 
+		mf.feature_id, 
 		mf.nws_station_id,
 		rfc_defined_fcst_point,
 		downstream_feature_id,
@@ -118,13 +49,12 @@ root_status_trace_reaches AS (
 		stream_length,
 		is_waterbody,
 		fcst_meta.issue_time,
-		status.rating_source,
-		INITCAP(status.status) as max_status
-	FROM max_flows_station_xwalk_with_rc_stage mf
+		INITCAP(rmf.max_status) as max_status
+	FROM max_flows_station_xwalk mf
 	LEFT JOIN fcst_meta
 		ON fcst_meta.lid = mf.nws_station_id
-	LEFT JOIN rnr.domain_lids_with_status status
-		ON status.lid = mf.nws_station_id
+	LEFT JOIN rnr.rfc_max_forecast_copy rmf
+		ON rmf.nws_lid = mf.nws_station_id
 	WHERE rfc_defined_fcst_point IS TRUE OR issue_time IS NOT NULL
 	ORDER BY feature_id, issue_time, rfc_defined_fcst_point DESC
 ),
@@ -141,7 +71,6 @@ status_trace AS (
 		stream_order AS root_stream_order,
 		stream_order,
 		max_status,
-		rating_source,
 		stream_length,
 		SUM(CAST(0 as float)) as distance_from_forecast_point
 	FROM root_status_trace_reaches
@@ -155,7 +84,6 @@ status_trace AS (
 		root_stream_order, 
 		stream_order, 
 		max_status,
-		rating_source,
 		stream_length
 
 	UNION
@@ -171,7 +99,6 @@ status_trace AS (
 		root.root_stream_order, 
 		iter.stream_order,
 		root.max_status,
-		root.rating_source,
 		iter.stream_length, 
 		root.distance_from_forecast_point + iter.stream_length as distance_from_forecast_point
 	FROM max_flows_station_xwalk iter
@@ -193,7 +120,7 @@ agg_trace AS (
 				WHEN max_status = 'All Thresholds Undefined'
 				THEN max_status || ' at ' || influential_forecast_point || ' (' || root_feature_id || ' [' || root_stream_order || ']) despite forecast issued ' || issue_time || ' ' || ROUND(CAST(distance_from_forecast_point * 0.000621 as numeric), 1) || ' miles upstream'
 				WHEN issue_time IS NOT NULL
-				THEN max_status || ' issued ' || issue_time || ' at ' || influential_forecast_point || ' (' || root_feature_id || ' [' || root_stream_order || ']) ' || ROUND(CAST(distance_from_forecast_point * 0.000621 as numeric), 1) || ' miles upstream'
+				THEN max_status || ' issued ' || issue_time || ' at ' || influential_forecast_point || ' (' || root_feature_id || ' [order ' || root_stream_order || ']) ' || ROUND(CAST(distance_from_forecast_point * 0.000621 as numeric), 1) || ' miles upstream'
 				ELSE max_status || ' at ' || influential_forecast_point || ' (' || root_feature_id || ' [' || root_stream_order || ']) ' || ROUND(CAST(distance_from_forecast_point * 0.000621 as numeric), 1) || ' miles upstream'
 			END,
 			'; '
@@ -222,6 +149,7 @@ SELECT DISTINCT ON (mf.feature_id)
 	mf.reference_time,
 	mf.streamflow,
 	mf.streamflow_cms,
+	fcst_meta.rating_curve_source,
 	mf.stream_order,
 	CASE WHEN agg.is_waterbody THEN 'yes' ELSE 'no' END AS is_waterbody,
 	CASE WHEN agg.is_below_waterbody THEN 'yes' ELSE 'no' END as is_downstream_of_waterbody,
@@ -238,4 +166,6 @@ FROM max_flows_station_xwalk mf
 LEFT JOIN agg_trace agg
 	ON agg.trace_feature_id = mf.feature_id
 LEFT JOIN derived.channels_conus channel 
-	ON channel.feature_id = mf.feature_id;
+	ON channel.feature_id = mf.feature_id
+LEFT JOIN fcst_meta
+	ON fcst_meta.lid = mf.nws_station_id;
