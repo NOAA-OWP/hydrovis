1a52382fc0b69854a83d78bc76a15c891f98c7a3
CatherineFitzpatrick-NOAA <49403615+cfitzpat479@users.noreply.github.com>
2024-03-19 11:28:02 -0500

    Create srf_water_management.sql

---
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/products/short_range/srf_water_management.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS publish.srf_water_management;
+
+SELECT
+    srf.feature_id,
+    srf.feature_id::text as feature_id_str,
+    CASE
+        WHEN reservoir_type = 1
+        THEN 'Level Pool'
+        WHEN reservoir_type = 2
+        THEN 'RFC'
+        WHEN reservoir_type = 3
+        THEN 'USACE'
+        WHEN reservoir_type = 4
+        THEN 'USGS'
+    END as reservoir_type,
+    srf.outflow,
+    srf.water_sfc_elev,
+    srf.reference_time
+INTO publish.srf_water_management
+FROM ingest.nwm_reservoir_srf as srf
+JOIN derived.nwm_reservoirs as reservoirs ON reservoirs.lake_id = srf.feature_id;
