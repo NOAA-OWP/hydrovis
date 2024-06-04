dcd151c9c1320b0a5f121243bec981d262c58ab6
Shawn Crawley <shawn.crawley@noaa.gov>
2024-06-04 12:41:00 -0600

    Removes "huc8" field from coastal extent services
    The huc8 field had already been removed from the ingest tables that all of
    the coastal extent services look at - however, the field hadn't been removed
    from a few dummy row inserts as well as the service .mapx files. Those issues
    are now resolved herein.

--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/fim_configs/mrf_nbm_3day_max_coastal_inundation_atlgulf_psurge.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/fim_configs/mrf_nbm_3day_max_coastal_inundation_atlgulf_psurge.sql
@@ -1,5 +1,5 @@
 INSERT INTO ingest.mrf_nbm_3day_max_coastal_inundation_atlgulf_psurge (
-	geom, reference_time, huc8)
+	geom, reference_time)
 	VALUES (NULL, to_char('1900-01-01 00:00:00'::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC'), '-9999');
 
 DROP TABLE IF EXISTS publish.mrf_nbm_3day_max_coastal_inundation_atlgulf_psurge;
