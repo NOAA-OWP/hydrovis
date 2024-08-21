d19a40d65cbd6f28e048d641f09243dd583ed788
katherine powell <katherine.powell@noaa.gov>
2024-08-21 17:24:03 +0000

    updated postprocessing sql file

--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/products/short_range_alaska/srf_15hr_rate_of_change_ak.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/products/short_range_alaska/srf_15hr_rate_of_change_ak.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS publish.srf_18hr_rate_of_change_ak;
+DROP TABLE IF EXISTS publish.srf_15hr_rate_of_change_ak;
 
 WITH roi AS (
     SELECT max_srf.feature_id, thresholds.high_water_threshold
@@ -23,10 +23,10 @@ SELECT
     round(((srf.streamflow * 35.315) - ana.discharge_cfs)::numeric, 2) as change_cfs,
     round((((srf.streamflow * 35.315) - ana.discharge_cfs)*100/ana.discharge_cfs)::numeric, 2) as change_perc,
     to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS update_time
-INTO publish.srf_18hr_rate_of_change_ak
+INTO publish.srf_15hr_rate_of_change_ak
 FROM ingest.nwm_channel_rt_srf_ak as srf
 JOIN roi ON roi.feature_id = srf.feature_id
 JOIN cache.max_flows_ana_past_hour_ak AS ana ON ana.feature_id = srf.feature_id
 JOIN derived.channels_alaska as channels ON channels.feature_id = srf.feature_id
-WHERE srf.forecast_hour IN (3,6,9,12,15,18)
+WHERE srf.forecast_hour IN (3,6,9,12,15)
 ORDER BY forecast_hour, srf.feature_id;
