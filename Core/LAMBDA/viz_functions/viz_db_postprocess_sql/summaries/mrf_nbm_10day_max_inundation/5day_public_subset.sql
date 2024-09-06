2a293118481b0da8189df1e60c1edb98ce54a8da
Shawn Crawley <shawn.crawley@noaa.gov>
2024-09-06 16:17:35 -0600

    Removes clipping from public_fim_domain subsetting Just filters on the channels that have public_fim_domain = TRUE;

--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/summaries/mrf_nbm_10day_max_inundation/5day_public_subset.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/summaries/mrf_nbm_10day_max_inundation/5day_public_subset.sql
@@ -1,10 +1,8 @@
--- We'll temporarily increase work_mem to 512MB, to help with performance on PostGIS spatial joins (default is 4MB)
-SET work_mem TO '1024MB';
 DROP TABLE IF EXISTS publish.mrf_nbm_max_inundation_5day_public;
 
 SELECT
     inun.feature_id_str,
-    ST_Intersection(inun.geom, fim_domain.geom) as geom,
+    inun.geom,
     inun.streamflow_cfs,
     inun.reference_time,
 	to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS update_time
@@ -12,8 +10,7 @@ INTO publish.mrf_nbm_max_inundation_5day_public
 FROM publish.mrf_nbm_max_inundation_5day as inun
 JOIN derived.channels_conus channels 
     ON channels.feature_id = inun.feature_id 
-    AND channels.public_fim_domain = TRUE
-JOIN derived.public_fim_domain AS fim_domain ON TRUE;
+    AND channels.public_fim_domain = TRUE;
 
 INSERT INTO publish.mrf_nbm_max_inundation_5day_public (
     reference_time, 
