78ce56d3cd2364029a9b86e3deb24c6e4b9a4b83
katherine powell <katherine.powell@noaa.gov>
2024-12-19 02:49:02 +0000

    corrected srf and mrf peak flow services to filter on high water threashold AND corrected service naming from alaska to _ak

--- Core/LAMBDA/viz_functions/viz_db_postprocess_sql/products/medium_range_blend_alaska/mrf_nbm_10day_peak_flow_arrival_time_ak.sql
+++ Core/LAMBDA/viz_functions/viz_db_postprocess_sql/products/medium_range_blend_alaska/mrf_nbm_10day_peak_flow_arrival_time_ak.sql
@@ -1,42 +1,35 @@
-DROP TABLE IF EXISTS publish.mrf_gfs_10day_peak_flow_arrival_time_alaska;
-
-WITH arrival_time AS(
-    SELECT 
-           forecasts.feature_id, 
-           max(forecasts.forecast_hour)+1 AS below_bank_return_hour
-    FROM ingest.nwm_channel_rt_mrf_gfs_ak_mem1 AS forecasts
-    GROUP BY forecasts.feature_id
-)
+DROP TABLE IF EXISTS publish.mrf_nbm_10day_peak_flow_arrival_time_alaska;
 
 SELECT
     forecasts.feature_id,
     forecasts.feature_id::TEXT AS feature_id_str,
+    channels.name,
+    channels.strm_order::integer,
     min(forecasts.forecast_hour) AS peak_flow_arrival_hour,
     to_char(forecasts.reference_time::timestamp without time zone + INTERVAL '1 hour' * min(forecasts.forecast_hour), 'YYYY-MM-DD HH24:MI:SS UTC') AS peak_flow_arrival_time,
+    channels.huc6,
+    'AK' as state,
     forecasts.nwm_vers,
     forecasts.reference_time,
     max_flows.discharge_cfs AS max_flow_cfs,
+    rf.high_water_threshold,
     arrival_time.below_bank_return_hour,
-    to_char(forecasts.reference_time::timestamp without time zone + INTERVAL '1 hour' * arrival_time.below_bank_return_hour, 'YYYY-MM-DD HH24:MI:SS UTC') AS below_bank_return_time,
+    arrival_time.below_bank_return_time,
     to_char(now()::timestamp without time zone, 'YYYY-MM-DD HH24:MI:SS UTC') AS update_time,
-    channels.strm_order::integer,
-    channels.name,
-    channels.huc6,
-    'AK' as state,
-    -9999.0 as high_water_threshold,
     channels.geom
    
-INTO publish.mrf_gfs_10day_peak_flow_arrival_time_alaska
-FROM ingest.nwm_channel_rt_mrf_gfs_ak_mem1 AS forecasts
+INTO publish.mrf_nbm_10day_peak_flow_arrival_time_alaska
+FROM ingest.nwm_channel_rt_mrf_nbm_ak AS forecasts
 
 -- Join in max flows on max streamflow to only get peak flows
-JOIN cache.max_flows_mrf_gfs_10day_ak AS max_flows
+JOIN cache.max_flows_mrf_nbm_10day_ak AS max_flows
     ON forecasts.feature_id = max_flows.feature_id AND round((forecasts.streamflow*35.315)::numeric, 2) = max_flows.discharge_cfs
 
 -- Join in channels data to get reach metadata and geometry
 JOIN derived.channels_alaska as channels ON forecasts.feature_id = channels.feature_id::bigint
 
--- Join in arrival_time 
-JOIN arrival_time ON forecasts.feature_id = arrival_time.feature_id
+-- Join in high water arrival time for return time (the yaml config file ensures that arrival time finishes first for this, but we'll join on reference_time as well to ensure)
+JOIN publish.mrf_nbm_10day_high_water_arrival_time_ak AS arrival_time ON forecasts.feature_id = arrival_time.feature_id and forecasts.reference_time = arrival_time.reference_time
 
+WHERE round((forecasts.streamflow*35.315)::numeric, 2) >= rf.high_water_threshold
 GROUP BY forecasts.feature_id, forecasts.reference_time, forecasts.nwm_vers, forecasts.streamflow, max_flows.discharge_cfs, arrival_time.below_bank_return_hour, channels.geom, channels.strm_order, channels.name, channels.huc6;
